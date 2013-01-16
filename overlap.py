8c36e338269c3936a97f275e6b50e2714fd2b11e
Javier Velazquez-Muriel <javi.velazquez@gmail.com>
2013-01-15 23:12:20 -0800

    Finished the calculation of all the overlaps in the set of sequences

--- overlap.py
+++ overlap.py
@@ -1,5 +1,8 @@
 
-
+import heapq
+import itertools
+import logging
+log = logging.getLogger("overlap")
 
 def characters_overlapping(left, right):
     """
@@ -26,7 +29,7 @@ def characters_overlapping(left, right):
         else:
             index1 = index1 + index2 - table[index2]
             if index2 > 0:
-                index2 = table(index2)
+                index2 = table[index2]
     return index2 # characters matched
 
 
@@ -52,85 +55,145 @@ def compute_back_track_table(seq):
     return table
 
 
+def build_contigs(seqs, overlaps):
+
+    # find the indices that are only in the left part of the overlaps.
+    # These are the indices where the contigs start
+    lefts = set()
+    rights = set()
+    for (n_chars, i, j) in overlaps:
+        rights.add(j)
+        lefts.add(i)
+        if i in rights:
+            lefts.remove(i)
+        if j in lefts:
+            lefts.remove(j)
+
+
 
 
 def assemble(seqs):
-
-    # sort sequences by length
-    lenghts = [(len(s),i) i,s for enumerate(seqs)]
+    log.info("Assemblying %s sequences", len(seqs))
+    # sort sequences by length. If there are overlaps between
+    # large sequences it will avoid calculating overlaps between
+    # smaller ones
+    lenghts = [(len(s),i) for i,s in enumerate(seqs)]
     lenghts.sort()
     lengths = lenghts.reverse()
-    sorted_seqs = [seqs(i) for l,i in lenghts]
-
-    # keep calculating the overlaps until there are no more
-    # overlaps or all fragments are assembled
-    max_overlap = -1
+    sorted_seqs = [seqs[i] for l,i in lenghts]
+    log.debug("Sorted sequences: %s", sorted_seqs)
     fragments_assembled = 0
-    assembly = []
-    n_seqs = len(seqs)
+    overlaps = []
+    n_seqs = len(sorted_seqs)
     mat = OverlapMatrix(n_seqs)
-    while max_overlap != 0 or fragments_assembled < len(n_seqs) -1:
-        compute_overlaps(seqs, mat)
-        pair, max_overlap = mat.get_max_overlapping_pair()
-        if pair:
-            assembly.append(pair)
+    while fragments_assembled < n_seqs - 1:
+        compute_overlaps(sorted_seqs, mat)
+        (n_chars, left_index, right_index) = mat.get_max_overlapping_pair()
+        if n_chars == 0:
+            break
+        else:
+            overlaps.append((n_chars, left_index, right_index))
             fragments_assembled += 1
+            log.debug("fragments assembled %s",fragments_assembled)
+    log.info("Assembly %s", overlaps)
+    contigs = build_contigs(sorted_seqs,overlaps)
 
 
-def compute_overlaps(seqs, mat)
-    for i in range(n_seqs):
-        if not mat.needs_calculation(i, len(seqs[i])):
+
+
+def compute_overlaps(seqs, mat):
+    log.info("Computing overlaps")
+    n_seqs = len(seqs)
+    for i, j in itertools.product(xrange(n_seqs), xrange(n_seqs)):
+        if i == j:
             continue
-        for j in range(i+1, n_seqs):
-            if not mat.needs_calculation(j, len(seqs[j])):
-                continue
-            if not mat.is_calculated(i,j):
-                mat.store(i,j, characters_overlapping(seqs[i], seqs[j]))
-            if not mat.is_calculated(j, i):
-                mat.store(j,i, characters_overlapping(seqs[j], seqs[i]))
-
-
+        if not mat.needs_calculation(i, j, len(seqs[i]), len(seqs[j])):
+            continue
+        n_chars = characters_overlapping(seqs[i], seqs[j])
+        log.debug("Overlap between sequences %s (left) and %s (right): %s",i, j, n_chars)
+        mat.store(i, j, n_chars)
 
 
 class OverlapMatrix:
 
     def __init__(self, n_sequences):
-        self.max_overlap = -1
-        self.max_pair = (-1,-1)
-        self.overlaps_dict = dict()
-        for i in range(n_seqs):
-            self.overlaps_dict[i] = dict()
+        self.overlaps_heap = []
+        self.start_dictionaries(n_sequences)
 
-    def store(self, i, j, n_overlap):
+    def start_dictionaries(self, n_seqs):
+        """ Build the dictionary used to check if the overlap
+        between 2 sequences has been calculated already
+
+            @param n_seqs Number of sequences considered
         """
-            i index of the left Sequence
-            j index of the right sequence
+        self.used_as_left = dict()
+        self.used_as_right = dict()
+        self.calculated = dict()
+        for i in xrange(n_seqs):
+            self.used_as_left[i] = False
+            self.used_as_right[i] = False
+            self.calculated[i] = dict()
+            for j in xrange(n_seqs):
+                self.calculated[i][j] = False
+
+    def store(self, i, j, n_chars):
+        """ Store the number of oerlapping characters between sequences i and j
+            @param i index of the sequence acting as "left"
+            @param j index of the sequence acting as "right"
+            @param n_chars Number of overlapping characters
         """
-        if i not in self.overlaps_dict:
-            self.overlaps_dict[i] = dict()
-        self.overlaps_dict[i][j] = n_overlap
-
-        if n_overlap > self.max_overlap:
-            self.max_overlap = n_overlap
-            self.max_pair = (i,j)
-
-    def is_calculated(self, i, j):
-        if i not in self.overlaps_dict:
-            return False
-        if j not in self.overlaps_dict[i]:
-            return False
-        return True
-
+        heapq.heappush(self.overlaps_heap, (-1 * n_chars, i, j))
+        self.calculated[i][j] = True
 
     def get_max_overlapping_pair(self):
-        if self.max_overlap <= 0:
-            return False, self.max_overlap
+        """ Return the next pair with the maximum overlap
+            Remove pairs that were calculated but need to be descarded
+            because they describe impossible overlaps (given the
+            previous overlaps)
+        """
+        try:
+            (n_chars, i, j) = heapq.heappop(self.overlaps_heap)
+            while self.used_as_left[i] == True and \
+                  self.used_as_right[j] == True:
+                (n_chars, i, j) = heapq.heappop(self.overlaps_heap)
+            self.mark_as_used(i,j)
+            return (-1 *n_chars, i, j)
+        except IndexError:
+            # if the heap fails is because there are no more overlaps
+            return (0,-1, -1)
 
 
-    def needs_calculation(self, i, length_seq):
-        if not i in self.overlaps_dict:
-            return False
-        if length_seq < self.max_overlap:
-            return False
-        else:
-            return True
+    def mark_as_used(self, left_index, right_index):
+        """ Mark that two sequences have been used already in one overlap
+            @param left_index Index of the sequence acting as left
+            @param rigth_index Index of the sequence acting as right
+        """
+        log.debug("Cleaning dictionary")
+        self.used_as_left[left_index] = True
+        self.used_as_right[right_index] = True
+
+
+    def needs_calculation(self, index_left, index_right, len_left, len_right):
+        """ Determines if the overlap between two sequences needs to be done.
+
+            1) If one sequence is shorter than the maximum overlap, there is
+            no need to calculate it now
+            2) If the overlap has been calculated already, do not do it again
+
+            @param index_let Index of the sequence acting as "left"
+            @param index_right Index of the sequence acting as "right"
+            @return True if is worth calculating the overlap between the
+            sequences i and j. False otherwise
+        """
+        log.debug("Check if calculating overlap (%s, %s) is required",
+                                                 index_left, index_right)
+        if len(self.overlaps_heap) == 0:
+            return True # nothing calculated yet
+        max_overlap = -1 * self.overlaps_heap[0][0]
+        if len_left < max_overlap or len_right < max_overlap:
+            return False # sequences too short to find larger maximum overlap
+        if self.used_as_left[index_left] or self.used_as_right[index_right]:
+            return False # the sequences have been assembled already as left or right
+        if self.calculated[index_left][index_right]:
+            return False # calculated already
+        return True
--- test.py
+++ test.py
@@ -10,19 +10,24 @@ import networkx as nx
 log = logging.getLogger("test_database")
 
 
-@unittest.skip("blah")
 class TestAssembleReads(unittest.TestCase):
 
     def setUp(self):
         self.fn_easy_case = "easy_case.txt"
 
-    @unittest.skip("blah")
     def test_easy_case(self):
         """ Test the assembly of the easy case """
-        text = debruijn.assemble(self.fn_easy_case)
-        solution = "O draconian devil! Oh lame saint!"
-        self.assertEqual(text, solution, msg="The assembly is incorrect")
+        f = open(self.fn_easy_case, "r")
+        for line in f:
+            reads = line.rstrip().split(";")
+            assembly = overlap.assemble(reads)
+            print assembly
+        f.close()
 
+#        solution = "O draconian devil! Oh lame saint!"
+#        self.assertEqual(text, solution, msg="The assembly is incorrect")
+
+    @unittest.skip("blah")
     def test_build_graph(self):
         """ Test building a basic deBruijn graph """
         G = debruijn.build_graph(self.fn_easy_case, 4)
@@ -45,21 +50,19 @@ class TestOverlap(unittest.TestCase):
         """
         seq1 = "abdc sdf "
         seq2 = "sdf sabd"
-        n_overlaps = overlap.max_overlap(seq1, seq2)
+        seq3 = "cc"
+        n_overlaps = overlap.characters_overlapping(seq1, seq2)
         self.assertEqual(n_overlaps, 4)
         ov = seq2[0:n_overlaps]
         self.assertEqual(ov, "sdf ")
-        n_overlaps = overlap.max_overlap(seq2, seq1)
+        n_overlaps = overlap.characters_overlapping(seq2, seq1)
         self.assertEqual(n_overlaps, 3)
         ov = seq1[0:n_overlaps]
         self.assertEqual(ov, "abd")
+        n_overlaps = overlap.characters_overlapping(seq2, seq3)
+        self.assertEqual(n_overlaps, 0)
 
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout)
     logging.root.setLevel(logging.DEBUG)
     unittest.main()
-
-
-
-abcdefghigdsfd
-sfd
